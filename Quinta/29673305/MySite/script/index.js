2017ab7634a12b8c731259a021511f4acf59f3bb
aluno <Aluno@PR01-2A-30.CSED.LAB>
2023-07-07 11:36:40 -0300

    Passo 1

---
+++ Quinta/29673305/MySite/script/index.js
@@ -0,0 +1,8 @@
+
+
+function toggleColor() {
+  var body = document.getElementById('body');
+  console.log('a')
+  body.classList.toggle('new-color');
+  
+}
---
+++ Quinta/31459676/meu site/JS/index.js
@@ -0,0 +1,6 @@
+function DarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+}
+
+  
